ed6c947ec169debe6ac3cc0c120b60721e4ef31d
Kenny <roccendalanda@gmail.com>
2023-09-29 10:40:29 +0800

    hihi

--- PokerGame/src/poker/Poker.java
+++ PokerGame/src/poker/Poker.java
@@ -58,7 +58,7 @@ public class Poker {
 	
 	public static void main(String args[]) {
 		System.out.println(new Poker().isFullHouse(
-			new String[] {"C2", "D2", "H2", "S3", "S4"}, 5));
+			new String[] {"C2", "D2", "H2", "S3", "S4"},5));
 	}
 
 }
